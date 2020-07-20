d8ae872ebcf31de86b8c49c2a8ee707f90140192
Kiril Vatev <vatev.1@gmail.com>
2020-07-20 17:39:40 -0400

    removing more unneeded test code

--- test.js
+++ test.js
@@ -1,13 +1,18 @@
+const { promisify } = require('util');
 const fs = require('fs');
 const path = require('path');
+
 const fileType = require('file-type');
-const pify = require('pify');
 const test = require('ava');
-const m = require('.');
+
+const toIco = require('.');
+
+const readdir = promisify(fs.readdir);
+const readFile = promisify(fs.readFile);
 
 test('generate ico', async t => {
-	const files = fs.readdirSync(path.join(__dirname, 'fixtures'));
-	const arr = await Promise.all(files.map(x => pify(fs.readFile)(path.join(__dirname, 'fixtures', x))));
+	const files = await readdir(path.join(__dirname, 'fixtures'));
+	const arr = await Promise.all(files.map(x => readFile(path.join(__dirname, 'fixtures', x))));
 
-	t.deepEqual(fileType(await m(arr)), {ext: 'ico', mime: 'image/x-icon'});
+	t.deepEqual(fileType(await toIco(arr)), {ext: 'ico', mime: 'image/x-icon'});
 });
